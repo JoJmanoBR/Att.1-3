ca4b50ec4d7c9d971574c4e57c5e1c2703e44b49
Leandro Ramalho Alves <leandro.ramalhoalves2008@gmail.com>
2026-04-02 20:07:54 -0300

    tecnologia

---
+++ src/componentes/dados/dados.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-dados',
+    templateUrl: 'dados.html',
+    styleUrl: 'dados.css',
+})
+
+export class dados {
+    name = 'GUILHERME';
+    numero = 17;
+    serie = '3° ano B Informática'
+}
